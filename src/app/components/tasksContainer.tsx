dbf96726139a239ccb802bc535e3b05082680ab9
DanielRPrado <ed.prado0607@gmail.com>
2024-10-22 16:42:03 -0600

    Cambios en container de tasks

--- src/app/components/tasksContainer.tsx
+++ src/app/components/tasksContainer.tsx
@@ -3,9 +3,15 @@ import "./tasksContainer.scss";
 export default function TasksContainer() {
     return (
         <div className="tasks-container">
-            <TaskItem />
-            <TaskItem />
-            <TaskItem />
+            <div className="top">
+                <h1>Tasks</h1>
+            </div>
+            <div className="list">
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+            </div>
+            
         </div>
     );
 }
