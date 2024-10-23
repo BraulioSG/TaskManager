5514387c4dd3cd089aa532d1fceaec064f5b96f7
DanielRPrado <ed.prado0607@gmail.com>
2024-10-23 08:34:48 -0600

    Creación de detalles, modificación de contenedor y del componente de task

--- src/app/components/taskItem.tsx
+++ src/app/components/taskItem.tsx
@@ -1,11 +1,18 @@
 import "./taskItem.scss";
-export default function TaskItem() {
+export interface TaskItemProps {
+      id: number;
+      title: string;
+      description: string;
+      important: boolean;
+      completed: boolean;
+  }
+export default function TaskItem(task : TaskItemProps) { 
     return (
         <div className="task-item">
             <div className="task-item_left">
-                <input type="checkbox" className="task-item_checkbox"/>
+                <input type="checkbox" className="task-item_checkbox" onClick={(e) => {e.stopPropagation()}}/>
                 <div className="task-item_title">
-                    <h3>Task Title</h3>
+                    <h3>{task.title}</h3>
                 </div>
             </div>
             <div className="task-item_right">
--- src/app/components/tasksContainer.tsx
+++ src/app/components/tasksContainer.tsx
@@ -1,17 +1,124 @@
-import TaskItem from "./taskItem";
+"use client";
+import { useState } from "react";
 import "./tasksContainer.scss";
+import TaskDetails from "./TaskDetails";
+import TaskItem,{  TaskItemProps } from "./taskItem";
+
 export default function TasksContainer() {
+    const [tasks, setTasks] = useState <TaskItemProps[]>([
+        {
+            id: 1,
+            title: "Tarea 1",
+            description: "Descripción de la tarea 1",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 2,
+            title: "Tarea 2",
+            description: "Descripción de la tarea 2",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 3,
+            title: "Tarea 3",
+            description: "Descripción de la tarea 3",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 4,
+            title: "Tarea 4",
+            description: "Descripción de la tarea 4",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 5,
+            title: "Tarea 5",
+            description: "Descripción de la tarea 5",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 6,
+            title: "Tarea 6",
+            description: "Descripción de la tarea 6",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 7,
+            title: "Tarea 7",
+            description: "Descripción de la tarea 7",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 8,
+            title: "Tarea 8",
+            description: "Descripción de la tarea 8",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 9,
+            title: "Tarea 9",
+            description: "Descripción de la tarea 9",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 10,
+            title: "Tarea 10",
+            description: "Descripción de la tarea 10",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 11,
+            title: "Tarea 11",
+            description: "Descripción de la tarea 11",
+            important: false,
+            completed: false,
+        },
+        {
+            id: 12,
+            title: "Tarea 12",
+            description: "Descripción de la tarea 12",
+            important: false,
+            completed: false,
+        },
+    ]);
+    const [selectedTask, setSelectedTask] = useState<TaskItemProps | null>(null);
     return (
-        <div className="tasks-container">
-            <div className="top">
-                <h1>Tasks</h1>
+        <div className="tasks-component">
+            <div className="tasks-container">
+                <div className="top">
+                    <h1>Tasks</h1>
+                </div>
+                <div className="list">
+                    {
+                        tasks.map((task) => (
+                            <div key={task.id} onClick={(e) => {e.stopPropagation();setSelectedTask(task)}} className="task"> 
+                                <TaskItem key={task.id} {...task} />
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="addTask">
+                    <input type="text" placeholder="+ Agregar una tarea" />
+                </div>
             </div>
-            <div className="list">
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-            </div>
-            
+            <aside className="sidebar">
+                {
+                    selectedTask ? (
+                        <TaskDetails />
+                    ) : null
+                    
+                }
+            </aside>
         </div>
     );
 }
