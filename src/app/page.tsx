5782ea5ff95d4a6c1a2c6445907e16a551669985
Braulio Solorio <soloriobraulio03@gmail.com>
2024-10-23 13:02:42 -0600

    page.tsx restored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,28 @@ import Sidebar from "@/app/components/Sidebar";
 
 import { TaskList } from "@/app/Models/Task";
 import { useState, createContext, useEffect } from "react";
-import { getPreferredTheme, getTaskLists } from "@/app/utils/Storage";
+import { getTaskLists } from "@/app/utils/Storage";
 
 
 import "./page.scss"
-import ThemeToggler from "./components/ThemeToggler";
 
 export const ListsContext = createContext([new TaskList("")])
 export const ActiveListContext = createContext(0);
 
 export default function Home() {
-  const [theme, setTheme] = useState<'light' | 'dark'>("dark");
   const [lists, setLists] = useState<TaskList[]>([]);
   const [activeIdx, setActiveIdx] = useState<number>(0);
 
   useEffect(() => {
-    setTheme(getPreferredTheme())
     setLists(getTaskLists());
   }, []);
 
-  console.log(theme);
   return (
     <ListsContext.Provider value={lists}>
       <ActiveListContext.Provider value={activeIdx}>
-        <div className={"main-container"} data-theme={theme}>
-          <ThemeToggler />
-          <Sidebar />
-          <TasksContainer />
+        <div className={"main-container"}>
+          <Sidebar setLists={setLists} setActiveIdx={setActiveIdx} />
+          <TasksContainer setLists={setLists} />
         </div>
       </ActiveListContext.Provider>
     </ListsContext.Provider>
