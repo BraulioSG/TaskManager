627bd0a6de97f2f37d5accaf38daa7b0ea3b5cf5
DanielRPrado <ed.prado0607@gmail.com>
2024-10-23 10:01:30 -0600

    Cambios en container y task

--- src/app/components/tasksContainer.tsx
+++ src/app/components/tasksContainer.tsx
@@ -1,97 +1,29 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./tasksContainer.scss";
 import TaskDetails from "./TaskDetails";
-import TaskItem,{  TaskItemProps } from "./taskItem";
+import { Task, TaskList } from "@/app/Models/Task";
+import TaskItem from "./taskItem";
 
 export default function TasksContainer() {
-    const [tasks, setTasks] = useState <TaskItemProps[]>([
-        {
-            id: 1,
-            title: "Tarea 1",
-            description: "Descripción de la tarea 1",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 2,
-            title: "Tarea 2",
-            description: "Descripción de la tarea 2",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 3,
-            title: "Tarea 3",
-            description: "Descripción de la tarea 3",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 4,
-            title: "Tarea 4",
-            description: "Descripción de la tarea 4",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 5,
-            title: "Tarea 5",
-            description: "Descripción de la tarea 5",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 6,
-            title: "Tarea 6",
-            description: "Descripción de la tarea 6",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 7,
-            title: "Tarea 7",
-            description: "Descripción de la tarea 7",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 8,
-            title: "Tarea 8",
-            description: "Descripción de la tarea 8",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 9,
-            title: "Tarea 9",
-            description: "Descripción de la tarea 9",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 10,
-            title: "Tarea 10",
-            description: "Descripción de la tarea 10",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 11,
-            title: "Tarea 11",
-            description: "Descripción de la tarea 11",
-            important: false,
-            completed: false,
-        },
-        {
-            id: 12,
-            title: "Tarea 12",
-            description: "Descripción de la tarea 12",
-            important: false,
-            completed: false,
-        },
-    ]);
-    const [selectedTask, setSelectedTask] = useState<TaskItemProps | null>(null);
+    const [tasks, setTasks] = useState <TaskList>(
+        new TaskList("Tasks")
+    );
+
+    useEffect(() => {
+        const initialTasks = new TaskList("Tasks");
+        initialTasks.addTask(new Task("First Task", "Description 1", "2022-01-01"));
+        initialTasks.addTask(new Task("Second Task", "Description 2", "2022-01-02"));
+        initialTasks.addTask(new Task("Third Task", "Description 3", "2022-01-03"));
+        initialTasks.addTask(new Task("Third Task", "Description 3", "2022-01-03"));
+        initialTasks.addTask(new Task("Third Task", "Description 3", "2022-01-03"));
+        initialTasks.addTask(new Task("Third Task", "Description 3", "2022-01-03"));
+        initialTasks.addTask(new Task("Third Task", "Description 3", "2022-01-03"));
+
+        setTasks(initialTasks);
+    }, []);
+
+    const [selectedTask, setSelectedTask] = useState<Task | null>(null);
     return (
         <div className="tasks-component">
             <div className="tasks-container">
@@ -100,9 +32,9 @@ export default function TasksContainer() {
                 </div>
                 <div className="list">
                     {
-                        tasks.map((task) => (
-                            <div key={task.id} onClick={(e) => {e.stopPropagation();setSelectedTask(task)}} className="task"> 
-                                <TaskItem key={task.id} {...task} />
+                        tasks.getTasks().pending.map((task, index) => (
+                            <div key={index} onClick={() => setSelectedTask(task)} className="task"> 
+                                <TaskItem task={task} />
                             </div>
                         ))
                     }
@@ -111,7 +43,7 @@ export default function TasksContainer() {
                     <input type="text" placeholder="+ Agregar una tarea" />
                 </div>
             </div>
-            <aside className="sidebar">
+            <aside className={`sidebar-details ${selectedTask ? "show" : ""}`}>
                 {
                     selectedTask ? (
                         <TaskDetails />
