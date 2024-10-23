6136e45d24b69ce3c1f49114e169e7befb84ba94
DanielRPrado <ed.prado0607@gmail.com>
2024-10-23 11:25:02 -0600

    Cambios en sidebar de detalles, tareas y container

--- src/app/components/taskItem.tsx
+++ src/app/components/taskItem.tsx
@@ -1,10 +1,14 @@
 import "./taskItem.scss";
 import { Task } from "@/app/Models/Task";
 export default function TaskItem({task}: {task: Task}) {
+    const handleCompletedChange = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        task.setCompleted(!task.isCompleted());
+    };
     return (
         <div className="task-item">
             <div className="task-item_left">
-                <input type="checkbox" className="task-item_checkbox" onClick={(e) => {e.stopPropagation()}}/>
+                <input type="checkbox" className="task-item_checkbox" onClick={handleCompletedChange} checked={task.isCompleted()}/>
                 <div className="task-item_title">
                     <h3>{task.getTitle()}</h3>
                 </div>
