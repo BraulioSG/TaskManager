1de0ae0e5b7a29ddcc69948490e80a0be8a0a0c6
DanielRPrado <ed.prado0607@gmail.com>
2024-10-23 16:13:14 -0600

    Más detalles en TaskDetails, modificaciones en el uso de información de Container y Item

--- src/app/components/taskItem.tsx
+++ src/app/components/taskItem.tsx
@@ -1,20 +1,35 @@
 import "./taskItem.scss";
 import { Task } from "@/app/Models/Task";
-export default function TaskItem({task}: {task: Task}) {
-    const handleCompletedChange = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        task.setCompleted(!task.isCompleted());
+
+export default function TaskItem({
+    task,
+    updateTask,
+}: {
+    task: Task;
+    updateTask: (task: Task) => void;
+}) {
+
+    const handleCompletedChange = () => {
+        const newCompletedState = !task.isCompleted(); 
+        task.setCompleted(newCompletedState); 
+        updateTask(task); 
     };
+
     return (
         <div className="task-item">
             <div className="task-item_left">
-                <input type="checkbox" className="task-item_checkbox" onClick={handleCompletedChange} checked={task.isCompleted()}/>
+                <input
+                    type="checkbox"
+                    className="task-item_checkbox"
+                    onClick={e => {handleCompletedChange(); e.stopPropagation();}}
+                    checked={task.isCompleted()} 
+                />
                 <div className="task-item_title">
                     <h3>{task.getTitle()}</h3>
                 </div>
             </div>
             <div className="task-item_right">
-            <input type="checkbox" id="important" className="task-item_important"/>
+                <input type="checkbox" id="important" className="task-item_important" />
                 <label htmlFor="important" className="important-label">
                     <div className="star unchecked"></div>
                     <div className="star checked"></div>
