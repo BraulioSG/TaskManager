df140fdcb41be0cb98f7f4bc76275fc6ccdc9094
DanielRPrado <ed.prado0607@gmail.com>
2024-10-24 03:50:54 -0600

    Cambios en taskItem

--- src/app/components/taskItem.tsx
+++ src/app/components/taskItem.tsx
@@ -1,44 +1,60 @@
 import "./taskItem.scss";
 import { Task } from "@/app/Models/Task";
+import { useId } from "react";
 
-export default function TaskItem({ task, updateTask, }: { task: Task; updateTask: (task: Task) => void; }) {
+export default function TaskItem({
+  task,
+  updateTask,
+}: {
+  task: Task;
+  updateTask: (task: Task) => void;
+}) {
 
-    const handleCompletedChange = () => {
-        task.setCompleted(!task.isCompleted());
-        updateTask(task);
-    };
+  const importantId = useId();
+  console.log(importantId);
 
-    const handleImportantChange = () => {
-        task.setIsImportant(!task.isImportant());
-        updateTask(task);
-    }
+  const handleCompletedChange = () => {
+    task.setCompleted(!task.isCompleted());
+    updateTask(task);
+  };
 
-    return (
-        <div className={`task-item ${task.isCompleted() ? "completed" : ""}`}>
-            <div className="task-item_left">
-                <input
-                    type="checkbox"
-                    className="task-item_checkbox"
-                    onChange={e => { e.preventDefault(); handleCompletedChange(); }}
-                    checked={task.isCompleted()}
-                />
-                <div className="task-item_title">
-                    <h3>{task.getTitle()}</h3>
-                </div>
-            </div>
-            <div className="task-item_right">
-                <input
-                    type="checkbox"
+  const handleImportantChange = () => {
+    task.setIsImportant(!task.isImportant());
+    updateTask(task);
+  };
 
-                    className="task-item_important"
-                    onChange={e => { e.preventDefault(); handleCompletedChange(); }}
-                    checked={task.isCompleted()}
-                />
-                <label htmlFor="important" className="important-label">
-                    <div className="star unchecked"></div>
-                    <div className="star checked"></div>
-                </label>
-            </div>
+  return (
+    <div className={`task-item ${task.isCompleted() ? "completed" : ""}`}>
+      <div className="task-item_left">
+        <input
+          type="checkbox"
+          className="task-item_checkbox"
+          onChange={(e) => {
+            e.preventDefault();
+            handleCompletedChange();
+          }}
+          checked={task.isCompleted()}
+        />
+        <div className="task-item_title">
+          <h3>{task.getTitle()}</h3>
         </div>
-    );
+      </div>
+      <div className="task-item_right">
+        <input
+          type="checkbox"
+          id={importantId} 
+          className="task-item_important"
+          onChange={(e) => {
+            e.stopPropagation();
+            handleImportantChange();
+          }}
+          checked={task.isImportant()}
+        />
+        <label htmlFor={importantId} className="important-label">
+          <div className="star unchecked"></div>
+          <div className="star checked"></div>
+        </label>
+      </div>
+    </div>
+  );
 }
--- src/app/components/tasksContainer.tsx
+++ src/app/components/tasksContainer.tsx
@@ -42,8 +42,7 @@ export default function TasksContainer({ setLists }: { setLists: (lists: TaskLis
         }
     };
 
-    // Cambia el tipo de e a React.MouseEvent<HTMLDivElement>
-    const handleOutsideClick = (e: React.MouseEvent<HTMLElement>) => {
+    const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
         if (!(e.target as Element).closest('.tasks-component') && !(e.target as Element).closest('.sidebar')) {
             setSelectedTask(null);
         }
